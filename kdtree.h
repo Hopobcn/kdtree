8373651369c068093f02fde1d325e96dbf7376e7
Pau Farre <hopobcn@gmail.com>
2016-09-19 00:17:06 +0200

    Include GTest includes and style changes

--- kdtree.h
+++ kdtree.h
@@ -7,6 +7,7 @@
 #include <boost/geometry.hpp>
 #include <boost/geometry/geometries/point_xy.hpp>
 
+
 namespace spatial_index {
 
 namespace util {
@@ -14,23 +15,31 @@ namespace util {
 // Some compile time recursion in order to get a dimension dynamically
 template <typename Point, std::size_t Dimension, std::size_t Count>
 struct dimension_extractor {
-    static inline typename boost::geometry::default_distance_result<Point>::type subtract(const Point &p1, const Point &p2, std::size_t dim) {
+    static inline typename boost::geometry::default_distance_result<Point>::type
+    subtract(const Point& p1, const Point& p2, std::size_t dim)
+    {
         if (Dimension == dim) {
             return boost::geometry::get<Dimension>(p1) - boost::geometry::get<Dimension>(p2);
         }
         return dimension_extractor<Point, Dimension + 1, Count>::subtract(p1, p2, dim);
     }
 };
+
 // end recursion
 template <typename Point, std::size_t Count>
 struct dimension_extractor<Point, Count, Count> {
-    static inline typename boost::geometry::default_distance_result<Point>::type subtract(const Point &p1, const Point &p2, std::size_t dim) {
+    static inline typename boost::geometry::default_distance_result<Point>::type
+    subtract(const Point& p1, const Point& p2, std::size_t dim)
+    {
     }
 };
 
 template <typename Point>
-typename boost::geometry::default_distance_result<Point>::type subtract(const Point &p1, const Point &p2, std::size_t dim) {
-    return dimension_extractor<Point, 0, boost::geometry::dimension<Point>::type::value>::subtract(p1, p2, dim);
+typename boost::geometry::default_distance_result<Point>::type
+subtract(const Point& p1, const Point& p2, std::size_t dim)
+{
+    return dimension_extractor<Point, 0, boost::geometry::dimension<Point>::type::value>::subtract(
+        p1, p2, dim);
 }
 
 } // namespace util
@@ -39,30 +48,41 @@ template <typename Data, typename Point = boost::geometry::model::d2::point_xy<d
 class kdtree {
 public:
     kdtree() {}
+
     virtual ~kdtree() {}
-    void add(const Point *point, const Data *data) {
+
+    void add(const Point* point, const Data* data)
+    {
         typename kdnode::ptr node = std::make_shared<kdnode>(point, data);
-        m_nodes.push_back(node);
+        m_nodes.push_back(std::move(node));
     }
-    void build() {
+
+    void build()
+    {
         if (m_nodes.empty()) {
             return;
         }
         m_root = build(m_nodes, 0);
     }
-    void clear() {
+
+    void clear()
+    {
         m_root.reset();
         m_nodes.clear();
     }
-    const Data *nearest_recursive(const Point &query) const {
+
+    const Data* nearest_recursive(const Point& query) const
+    {
         if (!m_root) {
-            return NULL;
+            return nullptr;
         }
         best_match best(m_root, std::numeric_limits<double>::max());
         nearest(query, m_root, best);
         return best.node->data;
     }
-    void knearest(const Point &query, size_t k, std::vector<const Data*> &result) const {
+
+    void knearest(const Point& query, size_t k, std::vector<const Data*>& result) const
+    {
         if (!m_root || k < 1) {
             return;
         }
@@ -76,9 +96,11 @@ public:
             tmp.pop();
         }
     }
-    const Data *nearest_iterative(const Point &query) const {
+
+    const Data* nearest_iterative(const Point& query) const
+    {
         if (!m_root) {
-            return NULL;
+            return nullptr;
         }
         MinPriorityQueue pq;
         best_match best(m_root, std::numeric_limits<double>::max());
@@ -98,54 +120,78 @@ public:
             }
             node_ptr near = dx <= 0 ? currentNode->left : currentNode->right;
             node_ptr far = dx <= 0 ? currentNode->right : currentNode->left;
-            if (far) pq.push(DistanceTuple(dx * dx, far));
-            if (near) pq.push(DistanceTuple(0, near));
+            if (far) {
+                pq.push(DistanceTuple(dx * dx, far));
+            }
+            if (near) {
+                pq.push(DistanceTuple(0, near));
+            }
         }
         return best.node->data;
     }
+
 private:
     struct kdnode {
         typedef std::shared_ptr<kdnode> ptr;
         ptr left;
         ptr right;
         int axis;
-        const Point *split;
-        const Data *data;
-        kdnode(const Point *g, const Data *d) : axis(0), split(g), data(d) {}
+        const Point* split;
+        const Data* data;
+
+        kdnode(const Point* g, const Data* d)
+            : axis(0),
+              split(g),
+              data(d) {}
     };
+
     typedef typename kdnode::ptr node_ptr; // get rid of annoying typename
     typedef std::vector<node_ptr> Nodes;
     typedef std::pair<double, node_ptr> DistanceTuple;
+
     struct SmallestOnTop {
-        bool operator()(const DistanceTuple &a, const DistanceTuple &b) const {
+        bool operator()(const DistanceTuple& a, const DistanceTuple& b) const
+        {
             return a.first > b.first;
         }
     };
+
     struct LargestOnTop {
-        bool operator()(const DistanceTuple &a, const DistanceTuple &b) const {
+        bool operator()(const DistanceTuple& a, const DistanceTuple& b) const
+        {
             return a.first < b.first;
         }
     };
+
     typedef std::priority_queue<DistanceTuple, std::vector<DistanceTuple>, SmallestOnTop> MinPriorityQueue;
     typedef std::priority_queue<DistanceTuple, std::vector<DistanceTuple>, LargestOnTop> MaxPriorityQueue;
     Nodes m_nodes;
     node_ptr m_root;
 
-    template<typename NODE_TYPE>
+    template <typename NODE_TYPE>
     struct Sort : std::binary_function<NODE_TYPE, NODE_TYPE, bool> {
-        Sort(std::size_t dim) : m_dimension(dim) {}
-        bool operator()(const NODE_TYPE &lhs, const NODE_TYPE &rhs) const {
+        Sort(std::size_t dim)
+            : m_dimension(dim) {}
+
+        bool operator()(const NODE_TYPE& lhs, const NODE_TYPE& rhs) const
+        {
             return util::subtract(*lhs->split, *rhs->split, m_dimension) < 0;
         }
+
         std::size_t m_dimension;
     };
+
     struct best_match {
         node_ptr node;
         double distance;
-        best_match(const node_ptr &n, double d) : node(n), distance(d) {}
+
+        best_match(const node_ptr& n, double d)
+            : node(n),
+              distance(d) {}
     };
 
-    node_ptr build(Nodes &nodes, int depth) {
+    node_ptr build(Nodes& nodes, int depth)
+    {
         if (nodes.empty()) {
             return node_ptr();
         }
@@ -163,7 +209,8 @@ private:
         return node;
     }
 
-    static void nearest(const Point &query, const node_ptr &currentNode, best_match &best) {
+    static void nearest(const Point& query, const node_ptr& currentNode, best_match& best)
+    {
         if (!currentNode) {
             return;
         }
@@ -181,8 +228,11 @@ private:
         }
         nearest(query, far, best);
     }
+
     template <typename PriorityQueue>
-    static void knearest(const Point &query, const node_ptr &currentNode, size_t k, PriorityQueue &result) {
+    static void
+    knearest(const Point& query, const node_ptr& currentNode, size_t k, PriorityQueue& result)
+    {
         if (!currentNode) {
             return;
         }
@@ -197,7 +247,7 @@ private:
         node_ptr near = dx <= 0 ? currentNode->left : currentNode->right;
         node_ptr far = dx <= 0 ? currentNode->right : currentNode->left;
         knearest(query, near, k, result);
-        if ((dx * dx)  >= result.top().first) {
+        if ((dx * dx) >= result.top().first) {
             return;
         }
         knearest(query, far, k, result);
